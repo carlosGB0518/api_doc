516954fdbedb015cd3845d6522f933343317cc27
carlosGB0518 <carlosgonzalez6789@hotmail.com>
2025-04-03 02:51:42 +0000

    clase_api

---
+++ src/componentes/detalle/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './style2.css'
+
+function Detalle() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Detalle</h1>
+    </>
+  )
+}
+
+export default Detalle
---
+++ src/componentes/listar/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './style4.css'
+
+function Listar() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Listar</h1>
+    </>
+  )
+}
+
+export default Listar
